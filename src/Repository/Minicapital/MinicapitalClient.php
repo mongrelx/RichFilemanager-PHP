dbac55152671d2781b860ebd548f5044ec5100d6
Matti-Oskari Leppänen <mongrelx@gmail.com>
2018-03-15 12:46:34 +0200

    Minicapital initial

---
+++ src/Repository/Minicapital/MinicapitalClient.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of MinicapitalClient
+ *
+ * @author Administrator
+ */
+
+namespace RFM\Repository\Minicapital;
+
+class MinicapitalClient {
+    
+     /**
+     * Storage constructor.
+     *
+     * @param array $config
+     */
+    public function __construct($config = [])
+    {
+        print_r($config);
+    }
+    //put your code here
+}
